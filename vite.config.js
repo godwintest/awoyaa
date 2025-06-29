6526a1b242abb55804afeba550e52a3585f74eef
godwintest <etornamkofi5@gmail.com>
2025-06-29 14:33:36 +0000

    Fixed Browser Routing

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -6,5 +7,21 @@ export default defineConfig({
   build: {
     outDir: "dist",
     assetsDir: "assets",
+    // New Fix Solution
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+        blog: resolve(__dirname, "blog.html"),
+        careers: resolve(__dirname, "careers.html"),
+        contact: resolve(__dirname, "contact.html"),
+        documentation: resolve(__dirname, "documentation.html"),
+        faq: resolve(__dirname, "faq.html"),
+        features: resolve(__dirname, "features.html"),
+        pricing: resolve(__dirname, "pricing.html"),
+        privacy: resolve(__dirname, "privacy.html"),
+        terms: resolve(__dirname, "terms.html"),
+      },
+    },
   },
 });
